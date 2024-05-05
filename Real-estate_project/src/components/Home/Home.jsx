61a4e1836f60fbe8942be2cd214d8c86af9a7ab1
Aayush701 <ayushghimire7011@gmail.com>
2024-05-05 17:12:36 +0545

    completed frontend

--- Real-estate_project/src/components/Home/Home.jsx
+++ Real-estate_project/src/components/Home/Home.jsx
@@ -1,7 +1,26 @@
 import React from "react";
+import HeroSection from "../../subComponents/HeroSection";
+import TopVillas from "../../subComponents/TopVillas";
+import Regions from "../../subComponents/Regions";
+import OurSpecialities from "../../subComponents/OurSpecialities";
+import About from "../../subComponents/About";
+import Host from "../../subComponents/Host";
+import Contact from "../Contact/Contact";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <>
+      <article className="page">
+        <HeroSection />
+        <TopVillas />
+        <Regions />
+        <OurSpecialities />
+        <Host />
+        <About />
+        <Contact />
+      </article>
+    </>
+  );
 };
 
 export default Home;
--- Real-estate_project/src/subComponents/OurSpecialities.jsx
+++ Real-estate_project/src/subComponents/OurSpecialities.jsx
@@ -10,31 +10,31 @@ const OurSpecialities = () => {
       id: 1,
       title: "Seafront",
       icon: <SiWeightsandbiases />,
-      subText: "Have a look"
+      subText: "Have a look",
     },
     {
       id: 2,
       title: "Pet Friendly",
       icon: <MdOutlinePets />,
-      subText: "Pets allowed"
+      subText: "Pets allowed",
     },
     {
       id: 3,
       title: "Electric Car",
       icon: <MdElectricCar />,
-      subText: "Change your car"
+      subText: "Change your car",
     },
     {
       id: 4,
       title: "Fitness/Gym",
       icon: <MdFitnessCenter />,
-      subText: "Work out"
+      subText: "Work out",
     },
     {
       id: 5,
       title: "Boat Morning",
       icon: <FaSailboat />,
-      subText: "Take a trip"
+      subText: "Take a trip",
     },
   ];
   return (
@@ -42,17 +42,17 @@ const OurSpecialities = () => {
       <section id="ourSpecialities">
         <h1>OUR PROPERTIES SPECIALITIES</h1>
         <div className="specialities_container">
-          {
-            specialities.map(element=>{
-              return(
-                <div className="card" key={element.id}>
-                    <div className="icon">{element.icon}</div>
-                    <div className="title_text">{element.title}</div>
-                    <div className="subtitle_text">{element.subText} <FaLongArrowAltRight/></div>
+          {specialities.map((element) => {
+            return (
+              <div className="card" key={element.id}>
+                <div className="icon">{element.icon}</div>
+                <div className="title_text">{element.title}</div>
+                <div className="subtitle_text">
+                  {element.subText} <FaLongArrowAltRight />
                 </div>
-              )
-            })
-          }
+              </div>
+            );
+          })}
         </div>
       </section>
     </>
