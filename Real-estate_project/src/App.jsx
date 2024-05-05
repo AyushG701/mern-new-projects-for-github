cbdb467853ac18fe2d7839a1b87896dc34920483
Aayush701 <ayushghimire7011@gmail.com>
2024-05-05 15:03:58 +0545

    navbar, footer completed

---
+++ Real-estate_project/src/App.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar/Navbar";
+import Home from "./components/Home/Home";
+import Footer from "./components/Footer/Footer";
+import "./App.css";
+
+const App = () => {
+  return (
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+        <Footer />
+      </Router>
+    </>
+  );
+};
+
+export default App;
